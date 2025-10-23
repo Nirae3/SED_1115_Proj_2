4515a61a5384f00a09e6a9bb4b8e18b1d4cf134a
Audrey Kamwanga <audreyk@Audreys-MacBook-Air.local>
2025-10-23 12:53:39 -0400

    message

--- i2c_recieve.py
+++ i2c_recieve.py
@@ -4,3 +4,8 @@ from machine import Pin, I2C
 i2c = I2C(2, scl=Pin(15), sda=Pin(14)) # (id, *, scl, sda, freq=400000, timeout=50000)
 
 i2c.scan() # scan for master pico
+
+if message.any():
+    print("")
+else:
+    print("waitig for messages")
