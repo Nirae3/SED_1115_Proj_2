1c86ee3768df5c4d90e5c7c16bf2cb9c85cfb974
Nirae3 <128517942+Nirae3@users.noreply.github.com>
2025-10-24 11:22:23 -0400

    Update i2c_send.py

--- i2c_send.py
+++ i2c_send.py
@@ -6,15 +6,35 @@ scan_addr = i2c.scan() # scan for connected devices, returns a list
 
 message = "here is a message" # prepare a message
 
+# handle address not found
 
 
-def send_message(message):
+
+try scan_addr:
+    print("I'll be sending to the following address: ",hex(scan_addr[0]))
+exception BlockingIOError as e:
+    if e.
+
+except OSError as e:
+    if e.ENXIO == errno.ENXIO:
+        print("No address found")
+    elif e.
+
+
+
+try scan_addr:
+    print ("I'll be sending to the following address: ",hex(scan_addr[0]))
+except
+
+def send_message(message): # need to handle empty string of a message
     try:
-        i2c.writeto(scan_addr[0], message.encode()) #(addr, buf, stop=True, /) buf = encoded message.
-        print("the message has been successfully send to address \n", hex(scan_addr[0]))
+        i2c.writeto(scan_addr[0], message.encode()) #write to address with encoded message
+        print("the message has been successfully send to address \n", )
     except OSError
         print("The master pico wasn't able to send the data")
     # need to add error case handling
+
+
 
 
 if scan_addr:
@@ -29,6 +49,9 @@ convert_message_to_morse_code()
 send_message(message)
     call convert_message_to_morse_code()
 
+
+
+I2C.readfrom(scan_addr[0], 7,)
 
 
 """"
