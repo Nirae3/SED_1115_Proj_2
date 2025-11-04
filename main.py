4b7b5bfff0fe4b3c8ddf9d3dc66527018ff25a1a
Nirae3 <ardiarkel3@gmail.com>
2025-11-04 10:57:35 -0500

    fixed the length output issue so that the string doesn't cut off before it has processed the whole morse code

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import time
 
 adcA = ADC(Pin(26))
 led1 = PWM(Pin(18), freq=(1000))
+expected_msg_len=0
 
 ################ INITIALIZE UART ##########################################
-try:
+try:   # Handles if there is something wrong with the UART protocol. ex. Pin configuration is wrong
     uart = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9))
     uart.init(bits=8, parity=None, stop=1)
     print("UART Initalized correctly")
@@ -14,19 +15,19 @@ except Exception as e:
     print(f"ERROR: failed to initialize UART. Please check your hardware configuration")
 
 ################ MORSE CODE DEFINITION  ########################################
+# converts from a string to messasge
+def string_to_morse(message):  
+    allowed_chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .," # checks for the string to contain only the following letters
 
-def string_to_morse(message):
-    allowed_chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "
-
-    try:
+    try: # handles to make sure that the message is alsways a string 
         message=str(message)
     except Exception:
         raise TypeError("Input couldn't be converted to a string to convert to morse code")
 
-    if len(message)>20:
+    if len(message)>20: # lenght must only be 20 chars to make sure that all the string is sent and recieved
         raise ValueError("Input must be less than 20 characters")
     for char in message:
-        if char.upper() not in allowed_chars:
+        if char.upper() not in allowed_chars: # checks if the characters are allowed
             raise ValueError("Input must only contain letters, numbers, and spaces")
 
     morse_dict = {
@@ -37,12 +38,12 @@ def string_to_morse(message):
         ',' : '--..--', '.' : '.-.-.-'
 
     }
-    morse_code = ' '.join(morse_dict.get(char.upper(), '') for char in message)
+    morse_code = ' '.join(morse_dict.get(char.upper(), '') for char in message)  # convert char to upper, joins the chars into a single strip
     #print(morse_code)
     return morse_code
 
 def morse_to_text(morse_code):
-    # Morse code dictionary (reversed for decoding)
+    # converts from morse code to message
     morse_dict = {
         '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H',  '..': 'I', '.---': 'J', 
         '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
@@ -52,13 +53,13 @@ def morse_to_text(morse_code):
     }
 
     # Split Morse code into words ("/" separates words)
-    words = morse_code.strip().split(' / ')
+    words = morse_code.strip().split(' / ') # split the words
     decoded_text = []
 
     for word in words:
         letters = word.split()
-        decoded_word = ''.join(morse_dict.get(letter, '') for letter in letters)
-        decoded_text.append(decoded_word)
+        decoded_word = ''.join(morse_dict.get(letter, '') for letter in letters) # takes individual morse code, looks at the dictionary, takes the letter,joins the letters
+        decoded_text.append(decoded_word) # appends the list as it loops through the morse code.
 
     #print(' '.join(decoded_text))
     return ' '.join(decoded_text)
@@ -66,38 +67,43 @@ def morse_to_text(morse_code):
 
 ################### SEND MESSAGE, ACKNOWLEDGE, RECIEVE MESSAGE #######################
 def send_message(message): # define a function to send message
+    global expected_msg_len
     encoded_message = string_to_morse(message)
+    expected_msg_len = len(encoded_message) # check the lenght of what we are sending
     uart.write(encoded_message + '\n')  
-
-'''def wait_for_ack(timeout=2):
-    start=time.ticks_ms()
-    while time.ticks_diff(time.ticks_ms(), start)< timeout*1000:
-        if uart.any():
-            data=uart.readline()
-            if data and data.decode().strip() == "ACK":   
-                print("ACK RECIEVED")                     
-                return True                               
-    print("NO ACK recieved")
-    return False
-
-'''
 
 
 #check for messages, if there is any messages, save to data, and print recieved
-def read_message():
-    if uart.any():
-        data = uart.read()
+
+def read_message(timeout_ms=5000):
+    global expected_msg_len
+    start_time = time.ticks_ms()
+    received_morse = ""
+    
+    while time.ticks_diff(time.ticks_ms(), start_time) < timeout_ms:
+        data = uart.readline() 
+        
         if data:
-            message=data.decode().strip()
-            print("Received:", message)
-            print("here is the decoded message", morse_to_text(message))
-            #uart.write("ACK\n")                          CODE FOR ACK
-            return message
+            received_morse += data.decode() # if you recieve a message, format it
+            current_len=len(received_morse.strip())
+            if current_len < expected_msg_len and expected_msg_len > 0:
+                print(f"WARNING: Only a fragment of message recieved ({current_len}/{expected_msg_len}). Waiting for more")
+            elif current_len >= expected_msg_len and expected_msg_len > 0: 
+                message=received_morse.strip()
+                print("Received:", message)
+                print("Here is the decoded message", morse_to_text(message))
+                expected_msg_len = 0 # after your'e done, clear up t he expected_msg_len
+                return message
+            
+        time.sleep_ms(20)
+    if expected_msg_len > 0:
+        expected_msg_len = 0
+        raise ValueError ("ERROR: Timeout. Message not recieved fully within the time limit")
+    
     return None
 
 
-        
-
+    
 while True: # always loop to keep checking for messages.
     read_message() # read the message always
 
@@ -105,9 +111,15 @@ while True: # always loop to keep checking for messages.
         user_message = input("Type a message: ") # takes input message
         if user_message.strip():
             send_message(user_message) # if there is input, send that message
+            print("Message sent, waiting for confermation/reply ...")
+            recieved_reply = read_message(timeout_ms=5000)
+            if recieved_reply is None:
+                raise ValueError("ERROR: Timeout reached. No reply was recieved from UART. Please check your hardware")
             #wait_for_ack()                             CODE FOR ACK
             #time.sleep(2)                              CODE FOR ACK
     except EOFError:
-        print("there was an error with the input") # otherwise throw an error
+        print("ERROR reading input") # otherwise throw an error
         pass
+    except ValueError as e:
+        print(f"COMMUNICATION FAILURE: {e}")
     time.sleep(0.5)
